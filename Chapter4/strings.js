925677e789cbfcdccb50d1f6fa274cf3399bc49f
Adi <imaditg@outlook.com>
2025-06-09 22:36:00 +0530

    Add files via upload

---
+++ Chapter4/strings.js
@@ -0,0 +1,12 @@
+let arr = "Hakunamatata";
+console.log(arr.slice(2,7));
+
+console.log(arr.indexOf('a'));
+
+console.log(arr.padStart(22,"0"));
+
+console.log(String(6).padStart(3,"0"));
+
+console.log(arr.repeat(3));
+
+console.log(arr.padEnd(22,"0"));

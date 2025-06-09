67025366585c4e0af878a36735f1314ae7ece825
Adi <imaditg@outlook.com>
2025-06-09 13:06:11 +0530

    Add files via upload

---
+++ Chapter4/checkingTypeof.js
@@ -0,0 +1,10 @@
+let na = "doh";
+console.log(typeof na.toUpperCase()); // it will show it is a string
+
+let arr = [1,2,3.4,5,6];
+
+console.log(arr.push(7));
+
+console.log(arr.push); // it will show,it is function
+console.log(arr.pop(4)); //it will only pop one elements irrespecty of parameter pass to pop function
+console.log(arr);

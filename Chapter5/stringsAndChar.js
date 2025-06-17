424c0ebaa31fc1adc9ebf2e94b9320cda2938f83
Adi <imaditg@outlook.com>
2025-06-17 22:20:48 +0530

    Add files via upload

---
+++ Chapter5/stringsAndChar.js
@@ -0,0 +1,17 @@
+let cr = '✌',nor = 'ABC';
+
+console.log(cr);
+
+console.log(typeof(cr));
+
+console.log(cr[0]);
+
+console.log(cr.length);
+
+console.log(cr.charCodeAt(0));
+
+console.log(cr.codePointAt(0));
+
+console.log(nor.charCodeAt());
+
+console.log(nor.codePointAt());

2e3d045070fad888791ea7bab4ebb246ef9c3147
Adi <imaditg@outlook.com>
2025-07-22 23:53:53 +0530

    Add files via upload

---
+++ Chapter9/boundries1.js
@@ -0,0 +1,9 @@
+console.log(/^\d+$/.test(22222));
+
+console.log(/^\d+$/.test("3333a"));
+
+console.log(/^!/.test("!hello"));
+
+console.log(/^!/.test("oppss!"));
+
+console.log(/^x/.test("hello"));

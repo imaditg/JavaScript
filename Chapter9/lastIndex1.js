0673cbdb065f428fcce4e5cc8f885cf898b55b12
Adi <imaditg@outlook.com>
2025-07-30 14:38:42 +0530

    Add files via upload

---
+++ Chapter9/lastIndex1.js
@@ -0,0 +1,21 @@
+let regx1 = new RegExp("c","g");
+
+let regx2 = new RegExp("c","y");
+
+regx1.lastIndex = 1;
+
+console.log(regx1.exec("abcd"));
+
+console.log(regx1.lastIndex);
+
+regx2.lastIndex = 1;
+
+console.log(regx2.exec("abcd"));
+
+console.log(regx2.lastIndex);
+
+regx2.lastIndex = 2;
+
+console.log(regx2.exec("abcd"))
+
+console.log(regx2.lastIndex);
